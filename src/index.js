739793155b1c34c85a9d34c6e6e131d5be34403b
realonereal <erohinreal@gmail.com>
2021-02-23 10:09:02 +0300

    fix:fix function in test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+ if(array.length == 0) {
+   return 0;
+}else {
+  array.sort((a,b) => (a-b));
+  return array[0];
+ }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array.length === 0) return 0;
+  array.sort((a,b) => (b-a));
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array.length === 0) return 0;
+  let sumArray = array.reduce((sum,current) => sum + current);
+
+  return sumArray/(array.length);
 }
