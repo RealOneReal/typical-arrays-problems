b13c42e367c7db45ea1f2e55f554f91bd8958e21
realonereal <erohinreal@gmail.com>
2021-03-02 15:41:21 +0300

    fix: fix code for empty array

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 
 exports.min = function min (array) {
- if(array.length == 0) {
+ if(!array || array.length === 0) {
    return 0;
-}else {
+} else {
   array.sort((a,b) => (a-b));
   return array[0];
  }
 }
 
 exports.max = function max (array) {
-  if(array.length === 0) return 0;
+  if(!array || array.length === 0) return 0;
   array.sort((a,b) => (b-a));
   return array[0];
 }
 
 exports.avg = function avg (array) {
-  if(array.length === 0) return 0;
+  if(!array || array.length === 0) return 0;
   let sumArray = array.reduce((sum,current) => sum + current);
 
   return sumArray/(array.length);
